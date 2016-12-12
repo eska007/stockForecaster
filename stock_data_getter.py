8f8a85fb8351a442ec387439b9c1da9024ad9db7
evan.kim <evan.kim@kaist.ac.kr>
2016-12-12 19:10:41 +0900

    Support to update stock data
    - append additional stock info to csv file

--- stock_data_getter.py
+++ stock_data_getter.py
@@ -21,6 +21,9 @@ stock_reg_kosdaq = './data/KOSDAQ.xls'
 
 columns = ['Open', 'High', 'Low', 'Close', 'Volume']
 
+default_data = np.empty((1, len(columns)))
+default_data[:] = np.NAN
+
 def get_info_with_web_scrap(item, df, idx_date):
     print("Get stock info using web scraping..")
     
@@ -114,9 +117,104 @@ def create_stock_csv_file(df, category, num=10):
 
     print("Complete to create init stock data!")
 
-def init_stock_data(df_kospi, df_kosdaq):  
-    create_stock_csv_file(df_kospi, "KRX", len(df_kospi))
-    create_stock_csv_file(df_kosdaq, "KOSDAQ", len(df_kosdaq))
+def init_stock_data(kospi, kosdaq):  
+    create_stock_csv_file(kospi, "KRX", len(kospi))
+    create_stock_csv_file(kosdaq, "KOSDAQ", len(kosdaq))
 
-# 전체 주식 데이터 생성 (최초 1회 수행)
-#init_stock_data(df_kospi, df_kosdaq)
+def update_stock_data(item, kospi, kosdaq):
+    try:
+        code = kospi[kospi['기업명'] == item]['종목코드'].values[0]
+        category = 'KRX'
+    except:
+
+        try:
+            code = kosdaq[kosdaq['기업명'] == item]['종목코드'].values[0]
+            category = 'KOSDAQ'
+        except:
+            sys.exit("ERROR - Unregisterd Stock!")
+
+    stock_item = str(code).zfill(6)
+    print(category, item, stock_item)
+
+    # 조회 종목 CSV 파일 확인
+    file_path = './data/csv/' 
+    file_name = category + '_' + stock_item + '.csv'
+
+    csv_file = True
+    if check_csv_file(category, stock_item):
+        print ("Exist csv file:" + file_name)
+        try:
+            df = pd.read_csv(file_path + file_name, index_col='Unnamed: 0')
+        except:
+            df = pd.read_csv(file_path + file_name, index_col='Date')
+            df = df.drop(df[-10:].index) # TEST1
+            #df = df.drop(['2016.12.06', '2016.12.07', '2016.12.08']) # TEST2
+    else:
+        df = DataFrame(default_data ,columns=columns, index=['1980.1.1'])
+        csv_file = False
+
+    df.index.name = 'Date'
+    # CSV 파일의 최종 날짜 정보와 조회시점의 정보 누락분 확인
+    #print("Check delta date")
+    d = [int(x) for x in df.index[-1].split('.')]
+    end_date = dtdt(d[0], d[1], d[2])
+    print(end_date)
+
+    check_hour = dtdt.now().hour
+    check_min = dtdt.now().minute
+
+    print(str(check_hour) + ':' + str(check_min))
+    market_closed = True
+    if check_hour == 15:
+        if (check_min > 30):
+            today = dt.date.today()
+    elif check_hour > 15:
+        today = dt.date.today()
+    else:
+        market_closed = False
+        today = dt.date.today() - dt.timedelta(days=1)
+
+    delta = dtdt(today.year, today.month, today.day) - end_date
+
+    # 구글 파이낸스에서 해당 종목 정보 받기
+    print("delta days:" + str(delta.days))
+    if delta.days <= 0 :
+        delta_dates = pd.date_range(end_date.strftime('%Y.%m.%d'), periods=1)
+    else:
+        delta_dates = pd.date_range(end_date.strftime('%Y.%m.%d'), periods=delta.days)
+    need_web_scrap = True
+    add_df = DataFrame(default_data, columns=columns)
+
+    if csv_file:
+        if delta.days > 30:
+            try:
+                add_df = data.DataReader(
+                    category + ":" + stock_item,
+                    "google",
+                    end_date,
+                    dtdt(today.year, today.month, today.day)
+                )
+                add_df.index =  pd.to_datetime(add_df.index).strftime('%Y.%m.%d')
+
+                need_web_scrap = False
+            except:
+                print ("Exception to get info via GOOGLE")
+
+
+    if(need_web_scrap):
+        add_df = get_info_with_web_scrap(stock_item, add_df, delta_dates)
+        add_df = add_df.dropna().sort_index()
+        if (market_closed):
+            print("Market closed")
+        else:
+            add_df.drop(add_df[-1:].index)
+        print("Complete to get stock info via NAVER")
+
+    if delta.days > 0 :
+        df = df.append(add_df).dropna()
+        #if not os.path.isfile(file_path+file_name):
+        df.to_csv(file_path+file_name)
+        #else:
+        #    df.to_csv(file_path+file_name, mode='a',header=True, index=True, index_label='Date')
+        
+
