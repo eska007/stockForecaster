c3c112d5373c678fdf99cd288e4627c2ef932b85
evan.kim <evan.kim@kaist.ac.kr>
2016-12-12 19:13:47 +0900

    Execute init and update stock data.

--- stock_data_mining.py
+++ stock_data_mining.py
@@ -4,7 +4,15 @@
 import stock_data_getter as sdg
 #sdg = __import__('stock_data_getter')
 
-# 생성 데이터 확인
+item = 'LG'
+
+category = ''
+df_kospi = sdg.pd.read_excel(sdg.stock_reg_kospi)
+df_kosdaq = sdg.pd.read_excel(sdg.stock_reg_kosdaq)
+
+# 주식 생성 데이터 확인
+#sdg.init_stock_data(df_kospi, df_kosdaq)
 sdg.init_stock_data(df_kospi, df_kosdaq)
 
-# 관심 종목 입력
+# 주식 데이터 업데이트
+sdg.update_stock_data(item, df_kospi, df_kosdaq)
